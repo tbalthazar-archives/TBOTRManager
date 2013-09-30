7d77e3eee63da90e96a217314d9d4401c7c37f54
Thomas Balthazar <thomas@balthazar.info>
2013-09-30 13:05:10 +0200

    Initial Commit

---
+++ TBOTRManager/TBOTRManager.h
@@ -0,0 +1,13 @@
+//
+//  TBOTRManager.h
+//  TBOTRManager
+//
+//  Created by Thomas Balthazar on 30/09/13.
+//  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBOTRManager : NSObject
+
+@end
---
+++ TBOTRManager/TBOTRManager.m
@@ -0,0 +1,13 @@
+//
+//  TBOTRManager.m
+//  TBOTRManager
+//
+//  Created by Thomas Balthazar on 30/09/13.
+//  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
+//
+
+#import "TBOTRManager.h"
+
+@implementation TBOTRManager
+
+@end
